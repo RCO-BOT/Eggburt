62b46f43b3f669382b809ecaca23afee67b62540
ValAlpha <lukeanthonysmith96@gmail.com>
2021-03-05 22:15:38 +0000

    temp

---
+++ commands/other/say.js
@@ -0,0 +1,23 @@
+const { Command } = require("discord.js-commando")
+
+module.exports = class say extends Command { 
+    constructor(client) {
+        super(client, {
+            name: "say", 
+            description: "The bot will repeat back whatever you ask it to", 
+            group: "other", 
+            memberName: "say", 
+            aliases: ["repeat", "echo"], 
+            args: [{
+                type: "string", 
+                prompt: "What do you want me to say?", 
+                key: "string"
+            }]
+        })
+    }
+    async run(msg, { string }){
+
+        msg.say(string).catch(err => console.log(err))
+
+    }
+}
